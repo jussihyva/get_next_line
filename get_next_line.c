dbee7f79c116d2eb4e704d42327f429f3820c335
Juhani Kauppi <jkauppi@student.hive.fi>
2019-11-09 15:19:46 +0200

    get_next_line.c modified to fullfill requirements in norminette.

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jkauppi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/31 10:24:46 by jkauppi           #+#    #+#             */
-/*   Updated: 2019/11/08 18:42:29 by jkauppi          ###   ########.fr       */
+/*   Updated: 2019/11/09 15:18:15 by jkauppi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,6 @@ static int			ft_build_new_line(char **buffer, char **line,
 			free(elem);
 			elem = tmp;
 		}
-		free(elem);
 		*elem_lst = NULL;
 		ft_strcat(*line, *buffer);
 	}
@@ -59,9 +58,12 @@ static size_t		ft_add_buf_lst(char *buffer,
 	return (index);
 }
 
-static int			ft_reply(char **buffer, size_t num_of_char,
+static int			ft_reply(t_fd_elem **fd_elem, size_t num_of_char,
 		char **line, t_list **elem)
 {
+	char		**buffer;
+
+	buffer = &(*fd_elem)->buffer;
 	*line = ft_memalloc((sizeof(char) * ft_strlen(*buffer) + num_of_char + 1));
 	if (**buffer || *elem)
 	{
@@ -69,18 +71,24 @@ static int			ft_reply(char **buffer, size_t num_of_char,
 					+ ft_strlen(*(buffer)), elem));
 	}
 	else
+	{
 		ft_memdel((void **)(buffer));
+		free(*fd_elem);
+		*fd_elem = NULL;
+	}
 	return (0);
 }
 
-static int			ft_read_fd_buffer(char **buffer, int fd, char **line,
+static int			ft_read_fd_buffer(t_fd_elem **fd_elem, int fd, char **line,
 		t_list **elem)
 {
 	ssize_t				index;
 	ssize_t				ret;
 	char				*match_ptr;
 	size_t				num_of_char;
+	char				**buffer;
 
+	buffer = &(*fd_elem)->buffer;
 	num_of_char = 0;
 	index = ft_strlen(*buffer);
 	while ((ret = read(fd, *buffer + index, BUFF_SIZE)) > 0)
@@ -88,8 +96,7 @@ static int			ft_read_fd_buffer(char **buffer, int fd, char **line,
 		*(char *)(*buffer + index + ret) = '\0';
 		if ((match_ptr = ft_strchr(*buffer + index, '\n')))
 		{
-			*line = ft_memalloc((sizeof(char) * ft_strlen(*buffer) +
-						num_of_char + 1));
+			*line = ft_memalloc(sizeof(char) * (index + ret + num_of_char + 1));
 			return (ft_build_new_line(buffer, line, match_ptr, elem));
 		}
 		index += ret;
@@ -99,14 +106,12 @@ static int			ft_read_fd_buffer(char **buffer, int fd, char **line,
 		*(*buffer + index + 1) = '\0';
 	if (ret)
 		return (ret);
-	ret = ft_reply(buffer, num_of_char, line, elem);
-	return (ret);
+	return (ft_reply(fd_elem, num_of_char, line, elem));
 }
 
 int					get_next_line(const int fd, char **line)
 {
 	char				*match_ptr;
-	ssize_t				ret;
 	t_list				*elem;
 	char				**buffer;
 	t_fd_elem			**fd_elem;
@@ -122,10 +127,7 @@ int					get_next_line(const int fd, char **line)
 			return (ft_build_new_line(buffer, line, match_ptr, &elem));
 		}
 		else
-		{
-			ret = (ft_read_fd_buffer(buffer, fd, line, &elem));
-			return (ret);
-		}
+			return (ft_read_fd_buffer(fd_elem, fd, line, &elem));
 	}
 	return (-1);
 }
